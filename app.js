21748a26c71b1b338c87eeb22ef9ff675a5901af
Xavier Tejeda <dosxxavier@Xaviers-iMac.cable.rcn.com>
2023-07-06 11:22:23 -0400

    Server Auth

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+app.use(express.json()); // for parsing application/json
+
+const crypto = require('crypto');
+
+// This is the in-memory store for this example.
+// In production, use a real database or cache system like Redis.
+const store = {};
+
+// This generates a random alphanumeric string of length 6.
+function generateAuthCode() {
+  return crypto.randomBytes(3).toString('hex');
+}
+
+// This is the first endpoint.
+app.post('/storeToken', (req, res) => {
+  const token = req.body.token;
+  if (!token) {
+    res.status(400).json({ error: 'No token provided' });
+    return;
+  }
+  const authCode = generateAuthCode();
+  store[authCode] = token;
+  res.json({ authCode: authCode });
+});
+
+// This is the second endpoint.
+app.get('/getToken/:authCode', (req, res) => {
+  const authCode = req.params.authCode;
+  const token = store[authCode];
+  if (!token) {
+    res.status(400).json({ error: 'Invalid auth code' });
+    return;
+  }
+  res.json({ token: token });
+});
+
+// This starts the server.
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
