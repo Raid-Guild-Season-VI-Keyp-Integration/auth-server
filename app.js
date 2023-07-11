2dc7305ea9d22f20bf2dd3e89bda2150ff0ef8d7
luxumbra <me@luxumbra.dev>
2023-07-11 01:59:52 +0100

    update cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:*', 'https://keypmine.luxumbra.dev'],
+  origin: ['http://localhost:3000', 'http://localhost:3420', 'https://keypmine.luxumbra.dev'],
 }))
 
 app.use(express.json()); // for parsing application/json
