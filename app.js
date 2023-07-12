065046c216d1b04214b4977c38f8728461b96661
luxumbra <me@luxumbra.dev>
2023-07-12 01:37:48 +0100

    not sure what happened here but it does explain why the server was restarting every time it was called. 😅

--- app.js
+++ app.js
@@ -1,23 +1,4 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3420', 'https://keypmine.luxumbra.dev'],
-}))
-
-app.use(express.json()); // for parsing application/json
-
-const crypto = require('crypto');
-
-// This is the in-memory store for this example.
-// In production, use a real database or cache system like Redis.
-const store = {};
-
-// This generates a random alphanumeric string of length 6.
-function generateAuthCode() {require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
@@ -88,41 +69,6 @@ app.get('/getInfo/:authCode', async (req, res) => {
     console.log('getInfo/authCode error', error.message)
     res.status(400).json({ error: error.message });
   }
-});
-
-// This starts the server.
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-  return crypto.randomBytes(3).toString('hex');
-}
-
-// This is the first endpoint.
-app.post('/storeToken', (req, res) => {
-  console.log('/storeToken req', req.body);
-  const token = req.body.token;
-  if (!token) {
-    res.status(400).json({ error: 'No token provided' });
-    return;
-  }
-  const authCode = generateAuthCode();
-  const address = req.body.address;
-  store[authCode] = token;
-  res.json({ authCode: authCode, walletAddress: address });
-});
-
-// This is the second endpoint.
-app.get('/getToken/:authCode', (req, res) => {
-  console.log('/getToken', req.params, req.body);
-  const authCode = req.params.authCode;
-  const address = req.params.address;
-  const token = store[authCode];
-  if (!token) {
-    res.status(400).json({ error: 'Invalid auth code' });
-    return;
-  }
-  res.json({ token: token, address: address });
 });
 
 // This starts the server.
